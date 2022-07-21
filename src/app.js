8228a713b5e14215819bff88a7ca1e886c4d7035
Your Name <santiago.perez777@gmail.com>
2022-07-21 15:20:07 +0200

    ok

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 const checkbox = document.querySelector(".checkbox");
 const body = document.querySelector(".body");
 const container = document.querySelectorAll(".light");
+const socialMedia = document.querySelectorAll(".social-media");
 
 checkbox.addEventListener("change", function () {
   if (this.checked) {
     body.classList.add("darkMode");
-
+    socialMedia.forEach((element) => {
+      element.classList.add("darkMode1");
+    });
     container.forEach((element) => {
       element.classList.add("darkMode1");
     });
   } else {
     body.classList.remove("darkMode");
-
+    socialMedia.forEach((element) => {
+      element.classList.remove("darkMode1");
+    });
     container.forEach((element) => {
       element.classList.remove("darkMode1");
     });
