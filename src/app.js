77fa9f4576dda917256db44dc61e498bbc0e96a2
Your Name <santiago.perez777@gmail.com>
2022-07-21 09:38:19 +0200

    ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const checkbox = document.querySelector(".checkbox");
 const body = document.querySelector(".body");
-const containers = document.querySelectorAll(".light");
+const container = document.querySelector(".container");
+const containers = container.getElementsByClassName("light");
 
 checkbox.addEventListener("change", function () {
   if (this.checked) {
     console.log("checked");
     body.classList.add("darkMode");
+    containers.classList.add("darkMode");
   } else {
     console.log("not checked");
     body.classList.remove("darkMode");
